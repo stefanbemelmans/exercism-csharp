183d1f63cd95e1ae1f82e3a83f6652700f622405
stackbaxter <stackbaxter@yahoo.com>
2019-06-15 08:46:49 -0500

    GradeSchool Complete, First Medium Challenge

--- grade-school/GradeSchool.cs
+++ grade-school/GradeSchool.cs
@@ -1,20 +1,48 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class GradeSchool
 {
+    public Dictionary<int, List<string>> studentsInGrades = new Dictionary<int, List<string>>();
+    
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+       if(studentsInGrades.ContainsKey(grade))
+       {
+            studentsInGrades[grade].Add(student);
+            studentsInGrades[grade].Sort();
+       }
+       else
+       {
+            studentsInGrades.Add(grade, new List<string> { student });
+       }
     }
 
     public IEnumerable<string> Roster()
-    {
-        throw new NotImplementedException("You need to implement this function.");
+    {  {
+        List<string> roster = new List<string>();
+        List<int> sortedGrades = new List<int>(studentsInGrades.Keys);
+        
+        sortedGrades.Sort();
+        foreach (int grade in sortedGrades)
+        {
+            roster.AddRange(studentsInGrades[grade]);
+        }
+        return roster;
+       }
     }
 
     public IEnumerable<string> Grade(int grade)
-    {
-        throw new NotImplementedException("You need to implement this function.");
+    {   
+        if(studentsInGrades.ContainsKey(grade))
+        { 
+            studentsInGrades[grade].Sort();
+            return studentsInGrades[grade];
+        }
+        else
+        {
+        return new List<string>();
+        }
     }
 }

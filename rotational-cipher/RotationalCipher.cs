6ce3b133202f6faecff67c8ce33014aec779e32e
stackbaxter <stackbaxter@yahoo.com>
2019-06-16 02:29:56 -0500

    cuz REFACTOR

--- rotational-cipher/RotationalCipher.cs
+++ rotational-cipher/RotationalCipher.cs
@@ -1,50 +1,39 @@
 using System;
-
+using System.Collections.Generic;
 public static class RotationalCipher
-{
+{   
+    
     public static string Rotate(string text, int shiftKey)
     { 
-        char letter;
+      bool IsCap;
+      char letter;
       char tempCap;
+      const char lastLetter = 'z';
+      const int AlphabetSize = 26;    
+      string excludedChars = " @0123456789!'?.,";
+      List<char> excludedCharsList = new List<char>(excludedChars);
       char[] textToArray = text.ToCharArray();
-      
-      if(shiftKey == 26) return text;
+     
+      if(shiftKey == AlphabetSize) return text;
           
       for (int i = 0; i < textToArray.Length; i++)
-      {
-        if(Char.IsUpper(textToArray[i]))
-        {
-          Console.WriteLine(textToArray[i]);
-          tempCap = Char.ToLower(textToArray[i]);
-          tempCap = (char)(tempCap + shiftKey);
-          Console.WriteLine(tempCap);
-         if(tempCap > 'z')
+      {  
+         letter = textToArray[i];
+       
+         if(!excludedCharsList.Contains(letter))
+         {
+            IsCap = Char.IsUpper(letter);
+            letter = IsCap ? Char.ToLower(letter) : letter; 
+           
+            letter = (char)(letter + shiftKey);
+            if(letter > lastLetter)
             {
-              tempCap = (char)(tempCap - 26);
-              Console.WriteLine(tempCap);
+              letter = (char)(letter - AlphabetSize);
             }
-          else 
-            {
-              tempCap = (char)(tempCap + 26);
-            }
-          Console.WriteLine(Char.ToUpper(tempCap));
-        }
-        else
-        {
-          letter = textToArray[i];
-          letter = (char)(letter + shiftKey);
-          
-          if(letter > 'z')
-            {
-              letter = (char)(letter - 26);
-            }
-          else
-            {
-              letter = (char)(letter + 26);
-            }
-          textToArray[i] = letter;
-        }
-      };
-        return new string(textToArray);
+            letter = IsCap ? Char.ToUpper(letter) : letter;
+            textToArray[i] = letter;
+         }
+      }
+      return new string(textToArray);
     }
 }

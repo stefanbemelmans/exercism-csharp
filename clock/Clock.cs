e5c8ee761f172ec9aab31e28af1fa1e5e4830abd
stackbaxter <stackbaxter@yahoo.com>
2019-06-24 23:34:29 -0500

    all tests pass, needs a litte optimizing

--- clock/Clock.cs
+++ clock/Clock.cs
@@ -31,46 +31,61 @@ public class Clock
 
     public Clock Add(int minutesToAdd)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new Clock(_H, _M + minutesToAdd);
     }
 
     public Clock Subtract(int minutesToSubtract)
     {
-        throw new NotImplementedException("You need to implement this function.");
+       return new Clock(_H, _M - minutesToSubtract);
     }
 
     public override string ToString()
     {
-      return string.Format("{0:D2}:{1:D2}", Hours + extraHours, Minutes);
+      return string.Format("{0:D2}:{1:D2}", Hours, Minutes);
     }
 
     public int convertMinutes(int min)
     {
         if(min == MinutesInHour)
         {   
-            extraHours =+ 1;
+            _H ++;
             return 0;
         };
         
         int totalMins = min;
+         if(totalMins < 0)
+        { 
+            while(totalMins < 0)
+            {  
+              totalMins += MinutesInHour;
+              _H --;
+            }
+            return totalMins;
+        };
+       
         if(totalMins > MinutesInHour)
-        {
-            extraHours =  totalMins / MinutesInHour;
-            if(extraHours > HoursInDay)
-            {
-              extraHours = extraHours % HoursInDay;
-            };
+        {    extraHours = totalMins / MinutesInHour;
+            _H = _H + extraHours;
             totalMins = totalMins % MinutesInHour; 
             return totalMins;
         }
+       
         return min;
     }
     public int convertHours(int hrs)
     {   
       
         if(hrs == HoursInDay){return 0;};
-        if(hrs < 0){return HoursInDay + hrs;};
         int totalHours = hrs;
+        if(totalHours < 0)
+        { 
+            while(totalHours < 0)
+            {  
+              totalHours += HoursInDay;
+            }
+            return totalHours;
+        };
+
         while(totalHours > HoursInDay)
         {
             totalHours = totalHours % HoursInDay;   
@@ -78,5 +93,17 @@ public class Clock
         }
         return (totalHours);
     }
-
+     public override bool Equals(object obj)
+       {
+         if ((obj == null) || ! this.GetType().Equals(obj.GetType())) 
+          {
+             return false;
+          }
+         else {
+            Clock clock = (Clock) obj;
+            return (Hours == clock.Hours && Minutes == clock.Minutes); 
+         }       
+     }      
+      
+   
 }   

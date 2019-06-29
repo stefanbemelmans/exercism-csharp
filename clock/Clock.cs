2b4437586c0180498240cb0b04d8b145f2ca45d2
stackbaxter <stackbaxter@yahoo.com>
2019-06-29 13:04:32 -0500

    Tests pass, could use a bit more refactoring, but okay for me for now.

--- clock/Clock.cs
+++ clock/Clock.cs
@@ -5,7 +5,6 @@ public class Clock
 {   int _H, _M;
     int HoursInDay = 24;
     int MinutesInHour = 60;
-    int extraHours = 0;
     int MinutesInDay = (24 * 60);
     public Clock(int hours, int minutes)
     {
@@ -18,8 +17,8 @@ public class Clock
     public int Hours
     {
         get
-        {   return _H;
-           // return convertHours(_H);
+        {
+            return _H;
         }
     }
 
@@ -28,7 +27,6 @@ public class Clock
         get
         {   
             return _M;
-            // return convertMinutes(_M);
         }
     }
 
@@ -47,32 +45,31 @@ public class Clock
       return string.Format("{0:D2}:{1:D2}", Hours, Minutes);
     }
 
-    public int TimePostivizor(int negTimeAmt, int timeToAdd)
+    public int TimePostivizor(int timeToRoll)
     {
-      int negativeTime = negTimeAmt;
-      while(negativeTime < 0)
+      int negativeMinutes = timeToRoll;
+      while(negativeMinutes < 0)
       {
-        negativeTime += timeToAdd;
+        negativeMinutes += MinutesInDay;
       }
-      return negativeTime;
+      return negativeMinutes;
     }
 
     public Dictionary<string,int> TimeReducer(int hours, int minutes)
     {
       Dictionary<string,int> Times = new Dictionary<string,int>();
       
-        int totalMinutes = ((hours * 60) + minutes);
-      //int totalMinutes = ((hours * 60) + minutes) % MinutesInDay;
-      if(totalMinutes < 0)
-      { 
-        totalMinutes = TimePostivizor(totalMinutes, MinutesInDay);; 
-      };
+      int totalMinutes = ((hours * 60) + minutes);
+          if(totalMinutes < 0)
+          { 
+            totalMinutes = TimePostivizor(totalMinutes); 
+          };
 
       int tempHour = totalMinutes / MinutesInHour;
-      if(tempHour > HoursInDay)
-      {
-        tempHour = tempHour % HoursInDay;
-      }
+           if(tempHour >= HoursInDay)
+           {
+            tempHour = tempHour %  HoursInDay;
+           }
 
       Times.Add("minutes",  totalMinutes % MinutesInHour);
       Times.Add("hours", tempHour);
@@ -80,55 +77,6 @@ public class Clock
       return Times;
     }
 
-    public int convertMinutes(int min)
-    {
-        if(min == MinutesInHour)
-        {   
-            _H ++;
-            return 0;
-        };
-        
-        int totalMins = min;
-         if(totalMins < 0)
-        { 
-            while(totalMins < 0)
-            {  
-              totalMins += MinutesInHour;
-              _H --;
-            }
-            return totalMins;
-        };
-       
-        if(totalMins > MinutesInHour)
-        {    extraHours = totalMins / MinutesInHour;
-            _H = _H + extraHours;
-            totalMins = totalMins % MinutesInHour; 
-            return totalMins;
-        }
-       
-        return min;
-    }
-    public int convertHours(int hrs)
-    {   
-      
-        if(hrs == HoursInDay){return 0;};
-        int totalHours = hrs;
-        if(totalHours < 0)
-        { 
-            while(totalHours < 0)
-            {  
-              totalHours += HoursInDay;
-            }
-            return totalHours;
-        };
-
-        while(totalHours > HoursInDay)
-        {
-            totalHours = totalHours % HoursInDay;   
-            convertHours(totalHours);
-        }
-        return (totalHours);
-    }
      public override bool Equals(object obj)
        {
          if ((obj == null) || ! this.GetType().Equals(obj.GetType())) 

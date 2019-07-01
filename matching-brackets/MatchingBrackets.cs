01937dcd6df34df276131f5605bf9fce44bf4440
stackbaxter <stackbaxter@yahoo.com>
2019-07-01 12:52:24 -0500

    passed all the tests and refactored enough for me.

--- matching-brackets/MatchingBrackets.cs
+++ matching-brackets/MatchingBrackets.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 public static class MatchingBrackets
-{
+{   
     public static bool IsPaired(string input)
+  { // Hit the last few tests and realized I was going about the wrong way. Too much info.
+
+    
+    Stack CurrentOPenBrackets = new Stack();
+    
+    Dictionary<char, char> Pairs = new Dictionary<char, char>
     {
-        throw new NotImplementedException();
+      {'{','}'},
+      {'(',')'},
+      {'[',']'},
+    }; 
+
+    foreach(var character in input)
+    { 
+      // looking for the closer
+      if(Pairs.ContainsValue(character))
+      {
+
+        if(CurrentOPenBrackets.Count <= 0)
+        {
+          return false;
+        }
+
+        char currentOpen = (char)CurrentOPenBrackets.Peek();
+        if(character != Pairs[currentOpen])
+        {
+          return false;
+        }
+        else{ 
+          CurrentOPenBrackets.Pop();
+        }         
+      }
+      if(Pairs.ContainsKey(character))
+      {
+        CurrentOPenBrackets.Push(character);
+    
+      }
+    
     }
+
+    return CurrentOPenBrackets.Count == 0 
+    ? true
+    : false
+    ;
+  }
+
 }
